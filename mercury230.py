8f9ae800e400578808b7d5c519a024327301413e
Philosof-k <77165321+Philosof-k@users.noreply.github.com>
2023-03-08 11:14:23 +0300

    Update mercury230.py

--- mercury230.py
+++ mercury230.py
@@ -554,7 +554,7 @@ class Mercury230:
         if outa[-2:] == self.crc16(outa[:-2])[-2:]:
             b1 = (outa[:-4])[-1:]
 #            bm = 0b00111111
-            b1 = b1 | 0b00111111
+            b1 = BitArray(b1) | BitArray(0b00111111)
             print(b1)
             b2 = (outa[:-2])[-1:]
             print(b2)
