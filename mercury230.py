d4eca0f6d03047935f3f74ebdb9531db2d4ee498
Philosof-k <77165321+Philosof-k@users.noreply.github.com>
2023-03-08 11:47:57 +0300

    Update mercury230.py

--- mercury230.py
+++ mercury230.py
@@ -554,7 +554,7 @@ class Mercury230:
         if outa[-2:] == self.crc16(outa[:-2])[-2:]:
             b1 = (outa[:-4])[-1:]
 #            bm = 0b00111111
-            b1 = b1 << 2
+            b1 = int(b1) << 2
             print(b1)
             b2 = (outa[:-2])[-1:]
             print(b2)
